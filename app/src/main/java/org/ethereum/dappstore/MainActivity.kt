501a8692606395fb09346c4bc153c4bca3e2655b
Joaquim Verges <joaquim.verges@gmail.com>
2021-12-04 20:13:07 -0800

    Bottom navigation screen switching

--- app/src/main/java/org/ethereum/dappstore/MainActivity.kt
+++ app/src/main/java/org/ethereum/dappstore/MainActivity.kt
@@ -3,6 +3,7 @@ package org.ethereum.dappstore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -10,20 +11,42 @@ import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavDirections
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import org.ethereum.dappstore.ui.components.AppListUi
 import org.ethereum.dappstore.ui.components.SearchBarUi
+import org.ethereum.dappstore.ui.components.SettingsUi
+import org.ethereum.dappstore.ui.components.UserUi
 import org.ethereum.dappstore.ui.theme.DAppStoreTheme
 import org.ethereum.dappstore.ui.theme.Selected
 import org.ethereum.dappstore.ui.theme.Unselected
 
 class MainActivity : ComponentActivity() {
+
+    sealed class Screen(val route: String, val icon: ImageVector) {
+        object Home : Screen("profile", Icons.Rounded.Home)
+        object User : Screen("user", Icons.Rounded.Person)
+        object Settings : Screen("settings", Icons.Rounded.Menu)
+    }
+
+    private val screens = listOf(Screen.Home, Screen.User, Screen.Settings)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
             DAppStoreTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
@@ -35,33 +58,28 @@ class MainActivity : ComponentActivity() {
                                 backgroundColor = MaterialTheme.colors.surface,
                                 elevation = 0.dp
                             ) {
-                                BottomNavigationItem(
-                                    selected = true,
-                                    onClick = { /*TODO*/ },
-                                    selectedContentColor = Selected,
-                                    unselectedContentColor = Unselected,
-                                    icon = {
-                                        Icon(Icons.Rounded.Home, contentDescription = "Home")
-                                    })
-                                BottomNavigationItem(
-                                    selected = false,
-                                    onClick = { /*TODO*/ },
-                                    selectedContentColor = Selected,
-                                    unselectedContentColor = Unselected,
-                                    icon = {
-                                        Icon(Icons.Rounded.Person, contentDescription = "Home")
-                                    })
-                                BottomNavigationItem(
-                                    selected = false,
-                                    onClick = { /*TODO*/ },
-                                    selectedContentColor = Selected,
-                                    unselectedContentColor = Unselected,
-                                    icon = {
-                                        Icon(Icons.Rounded.Menu, contentDescription = "Home")
-                                    })
+
+                                screens.forEach { screen ->
+                                    BottomNavigationItem(
+                                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                        onClick = { navController.navigate(screen.route) },
+                                        selectedContentColor = Selected,
+                                        unselectedContentColor = Unselected,
+                                        icon = {
+                                            Icon(screen.icon, contentDescription = screen.route)
+                                        })
+                                }
                             }
                         }) {
-                        AppListUi()
+
+                        NavHost(
+                            navController = navController,
+                            startDestination = Screen.Home.route
+                        ) {
+                            composable(Screen.Home.route) { AppListUi() }
+                            composable(Screen.User.route) { UserUi() }
+                            composable(Screen.Settings.route) { SettingsUi() }
+                        }
                     }
                 }
             }
---
+++ app/src/main/java/org/ethereum/dappstore/ui/components/SettingsUi.kt
@@ -0,0 +1,22 @@
+package org.ethereum.dappstore.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun SettingsUi() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Settings", style = MaterialTheme.typography.h5, fontWeight = FontWeight.Bold)
+    }
+}
