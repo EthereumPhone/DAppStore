6da098132ed23993a98175e65a59279ff31ed7f5
Markus Haas <haas.markus@protonmail.com>
2022-01-18 00:44:51 +0100

    Made downloading the apk more consistent
    The download works better now, I just chose pinata, but you can choose any ipfs gateway: https://ipfs.github.io/public-gateway-checker/

--- app/src/main/java/org/ethereum/dappstore/data/DataRepository.kt
+++ app/src/main/java/org/ethereum/dappstore/data/DataRepository.kt
@@ -69,8 +69,10 @@ object DataRepository {
 
     suspend fun fetchAppById(id: String): AppInfo? {
         return apolloClient.query(AppQuery(id)).await().data?.app?.let {
-            val img = ipfs.repo.ipfs.config.base_url + "cat?arg=" + it.logo
-            val apk = ipfs.repo.ipfs.config.base_url + "get?arg=" + it.appIPFSHash
+            //val img = ipfs.repo.ipfs.config.base_url + "cat?arg=" + it.logo
+            //val apk = ipfs.repo.ipfs.config.base_url + "get?arg=" + it.appIPFSHash
+            val img = "https://gateway.pinata.cloud/ipfs/" + it.logo
+            val apk = "https://gateway.pinata.cloud/ipfs/" + it.appIPFSHash
             AppInfo(
                 it.id,
                 it.appName,
